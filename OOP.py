887990648593c708f768a8fc074043719b5a95a6
Ivan <yivan922@gmail.com>
2019-09-28 18:47:36 +0700

    ооп классы

---
+++ OOP.py
@@ -0,0 +1,14 @@
+class Person():
+    def__init__(self, name, sername, money, hp, skorost):
+        self.name = name
+        self.sername = sername
+        self.money = money
+        self.hp = hp
+        self.skorost = skorost
+
+    def wazzep(self):
+        print("Меня зовут " + self.name + self.sername "имею" + self.money + self.hp +self.skorost)
+
+person1 = Person("Tom", "Redle", "1000", "1000", "1000")
+person1.wazzap()
+
