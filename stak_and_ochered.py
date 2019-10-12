31bb3d5ffcf06b9e3750e9f14c79aa44c83501ce
Ivan <yivan922@gmail.com>
2019-10-12 19:00:01 +0700

    ocheret

---
+++ stak_and_ochered.py
@@ -0,0 +1,82 @@
+# class Stack:
+
+#     def __init__(self):
+#         self.stack1 = []
+        
+#     def push(self, stack1):
+#         self.stack1.append(stack1)
+
+#     def pop(self):
+#         return self.stack1.pop()
+
+#     def peek(self):
+#         last = len(self.stack1) - 1
+
+class Queue:
+
+
+vopros1 = input("что вы хотите?")
+if vopros1 == "пенсия":
+    def __init__(self):
+        self.stack1 = []
+        
+    vopros2 = input("хотите ли вы выйти из очереди")
+    if vopros2 == "нет":
+        print ("ну как хочешь")
+    
+    if vopros2 == "да":
+        def push(self, stack1):
+            self.stack1.append(stack1)
+        print("надеюсь ты не вернёшся!")
+
+if vopros1 == "зарплата":
+    def __init__(self):
+        self.stack2 = []
+        
+    vopros2 = input("хотите ли вы выйти из очереди")
+    if vopros2 == "нет":
+        print ("ну как хочешь")
+    
+    if vopros2 == "да":
+        def push(self, stack1):
+            self.stack2.append(stack2)
+        print("надеюсь ты не вернёшся!")
+
+if vopros1 == "кредит":
+    def __init__(self):
+        self.stack3 = []
+        
+    vopros2 = input("хотите ли вы выйти из очереди")
+    if vopros2 == "нет":
+        print ("ну как хочешь")
+    
+    if vopros2 == "да":
+        def push(self, stack1):
+            self.stack3.append(stack3)
+        print("надеюсь ты не вернёшся!")
+
+if vopros1 == "займы":
+    def __init__(self):
+        self.stack4 = []
+        
+    vopros2 = input("хотите ли вы выйти из очереди")
+    if vopros2 == "нет":
+        print ("ну как хочешь")
+    
+    if vopros2 == "да":
+        def push(self, stack4):
+            self.stack4.append(stack4)
+        print("надеюсь ты не вернёшся!")
+
+if vopros1 == "другое":
+    def __init__(self):
+        self.stack5 = []
+        
+    vopros2 = input("хотите ли вы выйти из очереди")
+    if vopros2 == "нет":
+        print ("ну как хочешь")
+    
+    if vopros2 == "да":
+        def push(self, stack1):
+            self.stack5.append(stack5)
+        print("надеюсь ты не вернёшся!")
