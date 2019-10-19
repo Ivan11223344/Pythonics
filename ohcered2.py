17cc1cf6040350be01dfba2d0642f9d56a4d13e6
Ivan <yivan922@gmail.com>
2019-10-19 18:40:00 +0700

    очередь2

---
+++ ohcered2.py
@@ -0,0 +1,35 @@
+class Credit:
+    def __init__(self):
+        self.iteams =[]
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self,items):
+        self.iteams.insert(0,)
+
+# def info_of_item(self):
+#     print(self.iteam[])
+
+abc = kredet()
+
+print(abc.is_empty())
+
+print(abc.size())
+
+abc.enqueue("p02")
+abc.enqueue("p03")
+abc.enqueue("p04")
+abc.enqueue("p05")
+abc.enqueue("p06")
+abc.enqueue("p07")
+abc.enqueue("p08")
+
+ochered = 0
+# for y in pange(0,11):
+#     abc.anqueue("p0{}".format(random.randint(0,100)))
+
+print(abc.size())
+
+print(abc.info_of_item())
+
