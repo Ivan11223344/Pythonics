e785d140c1d7a6c45d17b7d6f79a7bbd36081c06
Ivan <yivan922@gmail.com>
2019-10-23 18:37:34 +0700

    очередь2

--- ohcered2.py
+++ ohcered2.py
@@ -1,35 +1,70 @@
 class Credit:
     def __init__(self):
-        self.iteams =[]
+        self.items = []
 
     def is_empty(self):
         return self.items == []
 
-    def enqueue(self,items):
-        self.iteams.insert(0,)
+    def enqueue(self, item):
+        self.items.insert(0, item)
 
-# def info_of_item(self):
-#     print(self.iteam[])
+    def dequeue(self):
+        return self.items.pop()
 
-abc = kredet()
+    def info_of_item(self, iteam):
+        print("индекс" + str(self.items.index(iteam)))
+
+        if self.items == 0:
+            print("wno")
+        if self.items == 1:
+            print("two")
+        if self.items == 2:
+            print("free")
+        if self.items == 3:
+            print("fore")
+        if self.items == 4:
+            print("fife")
+        if self.items == 5:
+            print("six")
+        if self.items == 6:
+            print("seven")
+        if self.items == 7:
+            print("eght")
+        if self.items == 8:
+            print("nite")
+        if self.items == 9:
+            
+
+
+    def info_of_queue(self):
+        print(self.items)
+
+    def size(self):
+        return len(self.items)
+
+
+abc = Credit()
 
 print(abc.is_empty())
 
 print(abc.size())
+print("===================")
 
-abc.enqueue("p02")
-abc.enqueue("p03")
-abc.enqueue("p04")
-abc.enqueue("p05")
-abc.enqueue("p06")
-abc.enqueue("p07")
-abc.enqueue("p08")
+# abc.enqueue("p02")
+# abc.enqueue("p03")
+# abc.enqueue("p04")
+# abc.enqueue("p05")
+# abc.enqueue("p06")
+# abc.enqueue("p07")
+# abc.enqueue("p08")
 
-ochered = 0
-# for y in pange(0,11):
-#     abc.anqueue("p0{}".format(random.randint(0,100)))
+for i in range(1, 11):
+    abc.enqueue("p-{}".format(i))
 
+print(abc.info_of_queue())
 print(abc.size())
+print("===================")
 
-print(abc.info_of_item())
+#print(abc.info_of_item())
 
+abc.info_of_item("p-6")
